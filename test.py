0f1a8bad93c303922bb70c123477a40b9b3dcc36
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-10-04 17:12:02 +0700

    Test

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 import joblib
 import matplotlib.pyplot as plt
 import pandas
-from sklearn.metrics import plot_confusion_matrix, precision_recall_fscore_support, plot_roc_curve, accuracy_score, \
-    plot_precision_recall_curve
+from sklearn.metrics import (
+    plot_confusion_matrix,
+    precision_recall_fscore_support,
+    plot_roc_curve,
+    accuracy_score,
+    plot_precision_recall_curve,
+    confusion_matrix,
+)
 
 from constants import *
 from train import get_model_file_name, get_test_file_name, get_vectorizer_file_name
+
+report_calculation_file = open("./report-extras/report-calculation.txt", "w")
 
 
 def get_test_result_file_name(fold):
@@ -38,32 +46,26 @@ plot_list = {
 
 for key in plot_list:
     roc_fig, roc_ax = plt.subplots()
-    plot_list[key]['roc_ax'] = roc_ax
-    plot_list[key]['roc_fig'] = roc_fig
+    plot_list[key]["roc_ax"] = roc_ax
+    plot_list[key]["roc_fig"] = roc_fig
 
     prc_fig, prc_ax = plt.subplots()
-    plot_list[key]['prc_ax'] = prc_ax
-    plot_list[key]['prc_fig'] = prc_fig
+    plot_list[key]["prc_ax"] = prc_ax
+    plot_list[key]["prc_fig"] = prc_fig
 
 for fold in range(0, N_FOLDS):
     test_results_per_fold = []
 
     for algorithm_id in ALGORITHMS:
-        model = joblib.load(
-            get_model_file_name(algorithm_id, fold)
-        )
+        model = joblib.load(get_model_file_name(algorithm_id, fold))
 
         test_file = pandas.read_csv(get_test_file_name(fold))
         data_test = test_file[DATA_KEY]
         target_test = test_file[TARGET_KEY]
 
-        tfidf_vectorizer = joblib.load(
-            get_vectorizer_file_name(fold)
-        )
+        tfidf_vectorizer = joblib.load(get_vectorizer_file_name(fold))
 
-        processed_data_test = tfidf_vectorizer.transform(
-            data_test
-        ).toarray()
+        processed_data_test = tfidf_vectorizer.transform(data_test).toarray()
 
         predicted_data_test = model.predict(processed_data_test)
 
@@ -72,33 +74,32 @@ for fold in range(0, N_FOLDS):
             model,
             processed_data_test,
             target_test,
-            labels=['f', 'h'],
-            display_labels=['Fakta', 'Hoax'],
-            cmap='Greys',
+            labels=["f", "h"],
+            display_labels=["Fakta", "Hoax"],
+            cmap="Greys",
         )
 
-        plt.ylabel('Kelas Prediksi')
-        plt.xlabel('Hasil Prediksi')
+        plt.ylabel("Kelas Prediksi")
+        plt.xlabel("Hasil Prediksi")
 
-        plt.savefig("./images/{}_{}_CONFUSION_MATRIX.png".format(
-            fold,
-            algorithm_id,
-        ))
+        plt.savefig(
+            "./images/{}_{}_CONFUSION_MATRIX.png".format(
+                fold,
+                algorithm_id,
+            )
+        )
         plt.clf()
         plt.close()
 
         # Plot and save ROC Curve
-        name = "{}{}".format(
-            ALGORITHM_SHORT_LABELS[algorithm_id],
-            fold + 1
-        )
+        name = "{}{}".format(ALGORITHM_SHORT_LABELS[algorithm_id], fold + 1)
 
         plot_roc_curve(
             model,
             processed_data_test,
             target_test,
             name=name,
-            ax=plot_list[algorithm_id]['roc_ax']
+            ax=plot_list[algorithm_id]["roc_ax"],
         )
 
         plot_precision_recall_curve(
@@ -106,64 +107,79 @@ for fold in range(0, N_FOLDS):
             processed_data_test,
             target_test,
             name=name,
-            ax=plot_list[algorithm_id]['prc_ax']
+            ax=plot_list[algorithm_id]["prc_ax"],
+        )
+
+        tn, fp, fn, tp = confusion_matrix(
+            target_test,
+            predicted_data_test,
+        ).ravel()
+
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        f1_score = 2 * precision * recall / (precision + recall)
+        accuracy = (tp + tn) / (tp + fp + tn + fn)
+
+        print(
+            """Untuk fold ke-{}, algoritma {}, jumlah true negative (tn) = {}, false positive (tp) = {}, false negative (fn) = {}; Dan true positive (tp) = {}. Maka nilai precision = tp / (tp + fp) =  {} / ({} + {})= {}; Nilai recall = tp / (tp + fn) = {} / ({} + {}) = {}; F1-score = 2 x precision x recall / (precision + recall) = 2 x {} x {} / ({} + {}) = {}; Nilai accuracy = tp + tn / (tp + fp + tn + fn) = {} + {} / ({} + {} + {} + {}) = {}.\n
+            """.format(
+                fold + 1, 
+                ALGORITHM_LABELS[algorithm_id],
+                tn, fp, fn, tp,
+                tp, tp, fp, precision,
+                tp, tp, fn, recall,
+                precision, recall, precision, recall, f1_score,
+                tp, tn, tp, fp, tn, fn, accuracy,
+            ).strip(
+            ),
+            file=report_calculation_file
         )
 
         precision, recall, f_score, support = precision_recall_fscore_support(
             target_test,
             predicted_data_test,
-            pos_label='h',
+            pos_label="h",
             zero_division=0,
-            average='binary',
+            average="binary",
         )
 
         accuracy = accuracy_score(target_test, predicted_data_test)
 
-        test_results_per_algorithm[algorithm_id].append({
-            "Precision": precision,
-            "Recall": recall,
-            "F1-Score": f_score,
-            "Accuracy": accuracy,
-        })
+        test_results_per_algorithm[algorithm_id].append(
+            {
+                "Precision": precision,
+                "Recall": recall,
+                "F1-Score": f_score,
+                "Accuracy": accuracy,
+            }
+        )
 
-        test_results_per_fold.append({
-            "Algoritma": ALGORITHM_LABELS[algorithm_id],
-            "Precision": precision,
-            "Recall": recall,
-            "F1-Score": f_score,
-            "Accuracy": accuracy,
-        })
+        test_results_per_fold.append(
+            {
+                "Algoritma": ALGORITHM_LABELS[algorithm_id],
+                "Precision": precision,
+                "Recall": recall,
+                "F1-Score": f_score,
+                "Accuracy": accuracy,
+            }
+        )
 
     pandas.DataFrame(
         test_results_per_fold,
-    ).to_excel(
-        get_test_result_file_name(fold + 1)
-    )
+    ).to_excel(get_test_result_file_name(fold + 1))
 
 for algorithm_id in plot_list:
-    roc_fig = plot_list[algorithm_id]['roc_fig']
-    roc_fig.savefig(
-        "./images/ROC_CURVE_{}.png".format(
-            algorithm_id
-        )
-    )
+    roc_fig = plot_list[algorithm_id]["roc_fig"]
+    roc_fig.savefig("./images/ROC_CURVE_{}.png".format(algorithm_id))
 
-    prc_fig = plot_list[algorithm_id]['prc_fig']
-    prc_fig.savefig(
-        "./images/PPC_CURVE_{}.png".format(
-            algorithm_id
-        )
-    )
+    prc_fig = plot_list[algorithm_id]["prc_fig"]
+    prc_fig.savefig("./images/PPC_CURVE_{}.png".format(algorithm_id))
 
 for algorithm_id, test_result in test_results_per_algorithm.items():
-    data_frame = pandas.DataFrame(
-        test_result
-    )
+    data_frame = pandas.DataFrame(test_result)
 
     mean = data_frame.mean()
 
     mean.to_excel(
-        get_test_result_file_name(
-            "Rata-Rata " + ALGORITHM_LABELS[algorithm_id]
-        )
+        get_test_result_file_name("Rata-Rata " + ALGORITHM_LABELS[algorithm_id])
     )
